c64c89e45373967276e510dd04bbada1b7f732f3
pappijx <ayush.papani123@gmail.com>
2023-04-13 00:30:14 +0530

    added tailwind config and tailwin

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,8 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="font-serif text-red-100">Web</h1>
+      <p className="font-bold">hello</p>
       <Button />
     </div>
   );
